289271e76942d3f4120e1ab23acbb88dea3c6c1e
Jaime Hernandez <jaimevazz@gmail.com>
2019-11-13 18:39:37 +0000

    Added UI to scene

---
+++ Assets/Scripts/CardImage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardImage : MonoBehaviour
+{
+    [SerializeField] Image image;
+    public void SetImage(Sprite sprite)
+    {
+        image.sprite = sprite;
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
