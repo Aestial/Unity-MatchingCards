95e7f37ac4b804af693744e7015f7496b2e8baa0
Jaime Hernandez <jaimevazz@gmail.com>
2019-11-13 18:40:03 +0000

    Added MatchesUIController

---
+++ Assets/Scripts/MatchesUIController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchesUIController : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] Transform container;
+    [SerializeField] PuzzleCreator creator;
+    
+    public void AddMatch(int type)
+    {
+        GameObject newMatch = Instantiate(prefab, container);
+        CardImage cardImage = newMatch.GetComponent<CardImage>();
+        cardImage.SetImage(creator.sprites[type].unlocked);
+    }
+    void Start()
+    {
+        
+    }
+}
