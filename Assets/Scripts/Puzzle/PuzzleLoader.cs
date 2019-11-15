f4d46a455b75f7d95c75291e647ba47175529c99
Jaime Hernandez <jaimevazz@gmail.com>
2019-11-15 20:27:27 +0000

    Splited PuzzleConstructor into Puzzle Loader and UI Builder

---
+++ Assets/Scripts/Puzzle/PuzzleLoader.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PuzzleLoader: MonoBehaviour
+{
+    // Persistence file
+    [SerializeField] string fileName;
+    string filePath;
+    // Build parameters
+    [Range(5, 35)] public int pairs;        
+    // Puzzle
+    Puzzle puzzle;
+    // Notifier
+    readonly Notifier notifier = new Notifier();
+    public const string ON_LOADED = "OnLoaded";
+    void Awake()
+    {        
+        filePath = Path.Combine(Application.persistentDataPath, fileName);
+    }
+    void Start()
+    {
+        PuzzleController controller = GetComponent<PuzzleController>();
+        puzzle = Get();        
+        notifier.Notify(ON_LOADED, puzzle);     
+        controller.Puzzle = puzzle;
+    }
+    void OnApplicationQuit()
+    {
+        Save();
+    }    
+    private void Save()
+    {
+        Debug.Log(filePath);
+        string json = JsonUtility.ToJson(puzzle);
+        File.WriteAllText(filePath, json);
+        Debug.Log(json);
+    }
+    private Puzzle Get()
+    {        
+        return File.Exists(filePath) ? Load() : Create();
+    }
+    private Puzzle Load()
+    {
+        string json = File.ReadAllText(filePath);
+        return JsonUtility.FromJson<Puzzle>(json);       
+    }
+    private Puzzle Create()
+    {
+        Card[] cards = CreateCards();
+        puzzle = new Puzzle(cards);
+        return puzzle;
+    }
+    private Card[] CreateCards()
+    {
+        List<Card> cards = new List<Card>();
+        for (int i = 0; i < pairs; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                Card newCard = new Card(i);
+                cards.Add(newCard);
+            }
+        }        
+        ListExtensions.Shuffle(cards);
+        return cards.ToArray();
+    }   
+}
---
+++ Assets/Scripts/Puzzle/UI/PuzzleUIBuilder.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleUIBuilder : MonoBehaviour
+{
+    // Card GameObjects
+    [SerializeField] GameObject cardPrefab;
+    // Placing parameters
+    [SerializeField] int columns;
+    [SerializeField] Vector2 spacing;
+    // Notifier
+    readonly Notifier notifier = new Notifier();
+    public const string ON_LOADED = "OnLoaded";
+
+    void Awake()
+    {
+        notifier.Subscribe(PuzzleLoader.ON_LOADED, HandleOnLoaded);
+    }
+    void OnDestroy()
+    {
+        notifier.UnsubcribeAll();
+    }
+    private void HandleOnLoaded(object[] args)
+    {
+        Puzzle puzzle = (Puzzle)args[0];
+        GameObject[] cardGOs = CreateCardsGO(puzzle.cards);
+        PlaceCards(cardGOs);        
+    }
+    private GameObject[] CreateCardsGO(Card[] cards)
+    {
+        List<GameObject> cardGOList = new List<GameObject>();
+        for (int i = 0; i < cards.Length; i++)
+        {
+            GameObject newCard = Instantiate(cardPrefab, transform);
+            CardController cc = newCard.GetComponent<CardController>();
+            cc.Card = cards[i];
+            cardGOList.Add(newCard);
+        }
+        return cardGOList.ToArray();
+    }
+    private void PlaceCards(GameObject[] gameObjects)
+    {
+        int length = gameObjects.Length;
+        for (int i = 0; i < length; i++)
+        {
+            Vector3 position = new Vector3(i % columns, i / columns);
+            position *= spacing;
+            gameObjects[i].transform.localPosition = position;
+        }
+        Vector3 offset = new Vector3((columns - 1) / 2.0f, (length - 1) / columns / 2.0f);
+        offset *= spacing;
+        transform.position -= offset;
+    }    
+}
