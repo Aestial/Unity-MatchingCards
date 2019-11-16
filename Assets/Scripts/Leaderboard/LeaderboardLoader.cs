aa12d0679d235012ea7154bbc65970555fc9b659
Jaime Hernandez <jaimevazz@gmail.com>
2019-11-16 13:11:44 +0000

    Added LeaderboardLoader

---
+++ Assets/Scripts/Leaderboard/LeaderboardLoader.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class LeaderboardLoader : MonoBehaviour
+{
+    // Persistence file
+    [SerializeField] string fileName;
+    string filePath;
+    Leaderboard leaderboard;
+    // Notifier
+    readonly Notifier notifier = new Notifier();
+    public const string ON_LEADERBOARD_LOADED = "OnLeaderboardLoaded";
+    void Awake()
+    {
+        filePath = Path.Combine(Application.persistentDataPath, fileName);
+    }
+    void Start()
+    {
+        LeaderboardController controller = GetComponent<LeaderboardController>();
+        leaderboard = Get();
+        notifier.Notify(ON_LEADERBOARD_LOADED, leaderboard);
+        controller.Leaderboard = leaderboard;
+    }
+    void OnApplicationQuit()
+    {
+        Save();
+    }    
+    private Leaderboard Get()
+    {
+        return File.Exists(filePath) ? Load() : Create();
+    }
+    private Leaderboard Load()
+    {
+        string json = File.ReadAllText(filePath);
+        return JsonUtility.FromJson<Leaderboard>(json);
+    }
+    private Leaderboard Create()
+    {
+        return new Leaderboard();
+    }
+    private void Save()
+    {
+        string json = JsonUtility.ToJson(leaderboard);
+        File.WriteAllText(filePath, json);
+        Debug.Log(json);
+    }
+}
