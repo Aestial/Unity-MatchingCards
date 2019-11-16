10a6559d15e301356d21cc61acdaca01a03a7b54
Jaime Hernandez <jaimevazz@gmail.com>
2019-11-16 13:12:40 +0000

    Update Leaderboard.cs

--- Assets/Scripts/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/Leaderboard/Leaderboard.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 [Serializable]
 public class Leaderboard
 {
-    public Game[] games;
+    //public Game[] games;
+    public List<Game> games;
 }
 
 [Serializable]
@@ -12,4 +14,10 @@ public class Game
     public User user;
     public int time;
     public int score;
+    public Game(User user, int score, int time)
+    {
+        this.user = user;        
+        this.score = score;
+        this.time = time;
+    }
 }
